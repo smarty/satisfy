22f71ec64646205447ffdf1e755bb21d0c917ce5
Gordon Mathis <gordon@smartystreets.com>
2020-01-09 14:29:47 -0700

    Disk file system implementation.

--- cmd/walk/main.go
+++ cmd/walk/main.go
@@ -1,36 +1,22 @@
 package main
 
 import (
-	"errors"
-	"fmt"
-	"io/ioutil"
+	"crypto/md5"
 	"log"
 	"os"
-	"path/filepath"
+
+	"bitbucket.org/smartystreets/satisfy/archive"
+	"bitbucket.org/smartystreets/satisfy/build"
+	"bitbucket.org/smartystreets/satisfy/fs"
 )
 
 func main() {
+	builder := build.NewPackageBuilder(fs.NewDiskFileSystem("/Users/Gordon/Desktop/bowling"),archive.NewTarArchiveWriter(os.Stdout),md5.New())
 
-	err := filepath.Walk("/Users/Gordon/src/bitbucket.org", walker)
+	err := builder.Build()
 	if err != nil {
 		log.Fatal(err)
 	}
-}
 
-func walker(path string, info os.FileInfo, err error) error {
-	if info.Name() == ".git" && info.IsDir() {
-		return filepath.SkipDir
-	}
-	fmt.Println(path, info.IsDir())
-	return errors.New("Hello")
-}
-
-func readDir() {
-	dir, err := ioutil.ReadDir("/Users/Gordon/src/bitbucket.org/smartystreets")
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, item := range dir {
-		fmt.Println(item.Name(), item.IsDir())
-	}
+	log.Println(builder.Contents())
 }
---
+++ fs/disk.go
@@ -0,0 +1,86 @@
+package fs
+
+import (
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+
+	"bitbucket.org/smartystreets/satisfy/contracts"
+)
+type FileInfo struct {
+	path string
+	size int64
+}
+
+func (this FileInfo) Path() string {
+	return this.path
+}
+
+func (this FileInfo) Size() int64 {
+	return this.size
+}
+
+type DiskFileSystem struct {
+	root string
+}
+
+func NewDiskFileSystem(root string) *DiskFileSystem {
+	return &DiskFileSystem{root: root}
+}
+
+func (this *DiskFileSystem) Listing() (listing []contracts.FileInfo) {
+	err := filepath.Walk(this.root, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		listing = append(listing, FileInfo{
+			path: path[len(this.root)+1:],
+			size: info.Size(),
+		})
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+	return listing
+}
+
+func (this *DiskFileSystem) Open(path string) io.ReadCloser {
+	reader, err := os.Open(this.absolute(path))
+	if err != nil {
+		log.Panic(err)
+	}
+	return reader
+}
+
+func (this *DiskFileSystem) absolute(path string) string {
+	return filepath.Join(this.root, path)
+}
+
+func (this *DiskFileSystem) Create(path string) io.WriteCloser {
+	writer, err := os.Create(this.absolute(path))
+	if err != nil {
+		log.Panic(err)
+	}
+	return writer
+}
+
+func (this *DiskFileSystem) ReadFile(path string) []byte {
+	raw, err := ioutil.ReadFile(this.absolute(path))
+	if err != nil {
+		log.Panic(err)
+	}
+	return raw
+}
+
+func (this *DiskFileSystem) WriteFile(path string, content []byte) {
+	err := ioutil.WriteFile(this.absolute(path), content, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+}
