e7335bb399068e4fc01238787202bb75c2b32a43
Gordon Mathis <gordon@smartystreets.com>
2020-01-09 10:40:39 -0700

    Package contents inventoried.

--- build/package_builder.go
+++ build/package_builder.go
@@ -1,25 +1,41 @@
 package build
 
 import (
+	"crypto/md5"
+	"hash"
+	"io"
+
 	"bitbucket.org/smartystreets/satisfy/contracts"
 )
 
 type PackageBuilder struct {
 	storage  contracts.FileSystem
 	archive  contracts.ArchiveWriter
+	contents []contracts.ArchiveItem
+	hasher   hash.Hash
 }
 
 func NewPackageBuilder(storage contracts.FileSystem, archive contracts.ArchiveWriter) *PackageBuilder {
 	return &PackageBuilder{
-		storage:  storage,
-		archive:  archive,
+		storage: storage,
+		archive: archive,
+		hasher:  md5.New(),
 	}
 }
 
 func (this *PackageBuilder) Build() error {
+	for _, file := range this.storage.Listing() {
+		reader := this.storage.Open(file.Path())
+		_, _ = io.Copy(this.hasher, reader)
+		this.contents = append(this.contents, contracts.ArchiveItem{
+			Path:        file.Path(),
+			Size:        file.Size(),
+			MD5Checksum: this.hasher.Sum(nil),
+		})
+	}
 	return nil
 }
 
 func (this *PackageBuilder) Contents() []contracts.ArchiveItem {
-	return nil
+	return this.contents
 }
--- build/package_builder_test.go
+++ build/package_builder_test.go
@@ -3,7 +3,11 @@ package build
 import (
 	"testing"
 
+	"github.com/smartystreets/assertions/should"
 	"github.com/smartystreets/gunit"
+
+	"bitbucket.org/smartystreets/satisfy/contracts"
+	"bitbucket.org/smartystreets/satisfy/fs"
 )
 
 func TestPackageBuilderFixture(t *testing.T) {
@@ -12,19 +16,38 @@ func TestPackageBuilderFixture(t *testing.T) {
 
 type PackageBuilderFixture struct {
 	*gunit.Fixture
-	builder *PackageBuilder
+	builder    *PackageBuilder
+	fileSystem contracts.FileSystem
+	archive    contracts.ArchiveWriter
 }
 
 func (this *PackageBuilderFixture) Setup() {
+	this.fileSystem = fs.NewInMemoryFileSystem()
+	this.archive = NewFakeArchiveWriter()
+	this.builder = NewPackageBuilder(this.fileSystem, this.archive)
 }
 
-func (this *PackageBuilderFixture) TestTarIsBuilt() {
+func (this *PackageBuilderFixture) TestArchiveContentsAreInventoried() {
+	this.fileSystem.WriteFile("file0.txt", []byte(""))
+	this.fileSystem.WriteFile("file1.txt", []byte("1"))
+	this.fileSystem.WriteFile("sub/file0.txt", []byte("12"))
 
+	err := this.builder.Build()
+	this.So(err, should.BeNil)
+	this.So(this.builder.Contents(), should.Resemble, []contracts.ArchiveItem{
+		{Path: "file0.txt", Size: 0, MD5Checksum: []uint8{212, 29, 140, 217, 143, 0, 178, 4, 233, 128, 9, 152, 236, 248, 66, 126}},
+		{Path: "file1.txt", Size: 1, MD5Checksum: []uint8{196, 202, 66, 56, 160, 185, 35, 130, 13, 204, 80, 154, 111, 117, 132, 155}},
+		{Path: "sub/file0.txt", Size: 2, MD5Checksum: []uint8{127, 111, 250, 166, 187, 11, 64, 128, 23, 182, 34, 84, 33, 22, 145, 181}},
+	})
 }
 
 /////////////////////////
 
 type FakeArchiveWriter struct {
+}
+
+func NewFakeArchiveWriter() *FakeArchiveWriter {
+	return &FakeArchiveWriter{}
 }
 
 func (this *FakeArchiveWriter) Write([]byte) (int, error) {
