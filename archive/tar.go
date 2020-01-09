c9ca96757fe50c7dd1eedf1b8687b9ba41a79218
Gordon Mathis <gordon@smartystreets.com>
2020-01-09 14:51:30 -0700

    Embedded the tar writer.

--- archive/tar.go
+++ archive/tar.go
@@ -7,24 +7,17 @@ import (
 )
 
 type TarArchiveWriter struct {
-	writer *tar.Writer
+	*tar.Writer
 }
 
 func NewTarArchiveWriter(writer io.Writer) *TarArchiveWriter {
-	return &TarArchiveWriter{writer: tar.NewWriter(writer)}
+	return &TarArchiveWriter{Writer: tar.NewWriter(writer)}
 }
 
 func (this *TarArchiveWriter) WriteHeader(name string, size int64) {
-	err := this.writer.WriteHeader(&tar.Header{Name:name, Size: size, Mode: 0644})
+	err := this.Writer.WriteHeader(&tar.Header{Name:name, Size: size, Mode: 0644})
 	if err != nil {
 		log.Panic(err)
 	}
 }
 
-func (this *TarArchiveWriter) Write(p []byte) (n int, err error) {
-	return this.writer.Write(p)
-}
-
-func (this *TarArchiveWriter) Close() error {
-	return this.writer.Close()
-}
