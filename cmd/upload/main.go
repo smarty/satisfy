f1408bff906f8c8494675a8535a953d4c9c357c3
Michael Whatcott <mdwhatcott@gmail.com>
2020-01-13 15:57:15 -0700

    Renamed the upload script folder.

--- cmd/upload/main.go
+++ cmd/upload/main.go
@@ -1,5 +1,179 @@
 package main
 
-func main() {
+import (
+	"bytes"
+	"compress/gzip"
+	"crypto/md5"
+	"encoding/json"
+	"hash"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 
+	"bitbucket.org/smartystreets/satisfy/archive"
+	"bitbucket.org/smartystreets/satisfy/build"
+	"bitbucket.org/smartystreets/satisfy/contracts"
+	"bitbucket.org/smartystreets/satisfy/fs"
+	"bitbucket.org/smartystreets/satisfy/remote"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	NewApp(parseConfig()).Run()
+	log.Println("OK")
+}
+
+type App struct {
+	config     Config
+	file       *os.File
+	hasher     hash.Hash
+	compressor *gzip.Writer
+	builder    *build.PackageBuilder
+	manifest   contracts.Manifest
+	uploader   contracts.Uploader
+}
+
+func NewApp(config Config) *App {
+	return &App{config: config}
+}
+
+func (this *App) Run() {
+	log.Println("Building the archive...")
+
+	this.buildArchiveAndManifestContents()
+
+	this.completeManifest()
+
+	this.buildUploader()
+
+	log.Println("Uploading the archive...")
+
+	this.upload(this.buildArchiveUploadRequest())
+
+	this.closeArchiveFile()
+
+	log.Println("Uploading the manifest...")
+
+	this.upload(this.buildManifestUploadRequest())
+
+	log.Println("Cleaning up...")
+
+	this.deleteLocalArchiveFile()
+}
+
+func (this *App) buildArchiveUploadRequest() contracts.UploadRequest {
+	this.openArchiveFile()
+	return contracts.UploadRequest{
+		Path:        this.config.composeRemotePath("tar.gz"),
+		Body:        NewFileWrapper(this.file),
+		Size:        int64(this.manifest.Archive.Size),
+		ContentType: "application/gzip",
+		Checksum:    this.manifest.Archive.MD5Checksum,
+	}
+}
+
+func (this *App) buildArchiveAndManifestContents() {
+	var err error
+	this.file, err = ioutil.TempFile("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	this.hasher = md5.New()
+	writer := io.MultiWriter(this.hasher, this.file)
+	this.compressor = gzip.NewWriter(writer)
+
+	this.builder = build.NewPackageBuilder(
+		fs.NewDiskFileSystem(this.config.sourceDirectory),
+		archive.NewTarArchiveWriter(this.compressor),
+		md5.New(),
+	)
+
+	err = this.builder.Build()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = this.compressor.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	this.closeArchiveFile()
+}
+
+func (this *App) buildManifestUploadRequest() contracts.UploadRequest {
+	buffer := this.writeManifestToBuffer()
+	return contracts.UploadRequest{
+		Path:        this.config.composeRemotePath("json"),
+		Body:        bytes.NewReader(buffer.Bytes()),
+		Size:        int64(buffer.Len()),
+		ContentType: "application/json",
+		Checksum:    this.hasher.Sum(nil),
+	}
+}
+
+func (this *App) buildUploader() {
+	client := &http.Client{Timeout: time.Minute}
+	gcsUploader := remote.NewGoogleCloudStorageUploader(client, this.config.googleCredentials, this.config.remoteBucket)
+	this.uploader = remote.NewRetryUploader(gcsUploader, 5)
+}
+
+func (this *App) completeManifest() {
+	fileInfo, err := os.Stat(this.file.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+	this.manifest = contracts.Manifest{
+		Name:    this.config.packageName,
+		Version: this.config.packageVersion,
+		Archive: contracts.Archive{
+			Filename:    filepath.Base(this.config.composeRemotePath("json")),
+			Size:        uint64(fileInfo.Size()),
+			MD5Checksum: this.hasher.Sum(nil),
+			Contents:    this.builder.Contents(),
+		},
+	}
+}
+
+func (this *App) closeArchiveFile() {
+	err := this.file.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (this *App) deleteLocalArchiveFile() {
+	err := os.Remove(this.file.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (this *App) openArchiveFile() {
+	var err error
+	this.file, err = os.Open(this.file.Name())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (this *App) upload(request contracts.UploadRequest) {
+	err := this.uploader.Upload(request)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (this *App) writeManifestToBuffer() *bytes.Buffer {
+	buffer := new(bytes.Buffer)
+	this.hasher.Reset()
+	writer := io.MultiWriter(buffer, this.hasher)
+	encoder := json.NewEncoder(writer)
+	encoder.SetIndent("", "  ")
+	_ = encoder.Encode(this.manifest)
+	return buffer
 }
