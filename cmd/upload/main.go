f5111636500484ad1caf79ec83552804684f6c86
Michael Whatcott <mdwhatcott@gmail.com>
2020-01-13 16:38:25 -0700

    TODO

--- cmd/upload/main.go
+++ cmd/upload/main.go
@@ -20,6 +20,8 @@ import (
 	"bitbucket.org/smartystreets/satisfy/remote"
 )
 
+// TODO: if manifest is already on remote storage, don't upload anything.
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	NewApp(parseConfig()).Run()
