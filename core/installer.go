0f4114a0ae76690fe5ed88ddf18b69fed39454b1
Gordon Mathis <gordon@smartystreets.com>
2020-01-16 16:50:15 -0700

    Finished installing the manifest.

--- core/installer.go
+++ core/installer.go
@@ -2,6 +2,9 @@ package core
 
 import (
 	"encoding/json"
+	"fmt"
+	"path/filepath"
+	"strings"
 
 	"bitbucket.org/smartystreets/satisfy/contracts"
 )
@@ -11,11 +14,20 @@ type PackageInstaller struct {
 	filesystem contracts.FileSystem
 }
 
-func (this *PackageInstaller) InstallManifest(request contracts.InstallationRequest) (contracts.Manifest, error) {
-	readcloser, _ := this.downloader.Download(request.DownloadRequest)
+func (this *PackageInstaller) InstallManifest(request contracts.InstallationRequest) (manifest contracts.Manifest, err error) {
+	readcloser, err := this.downloader.Download(request.DownloadRequest)
+	if err != nil {
+		return manifest, err
+	}
 	decoder := json.NewDecoder(readcloser)
-	var manifest contracts.Manifest
-	decoder.Decode(&manifest)
+	err = decoder.Decode(&manifest)
+	if err != nil {
+		return manifest, err
+	}
+	file := this.filesystem.Create(filepath.Join(request.LocalPath, fmt.Sprintf("manifest_%s_%s.json", strings.ReplaceAll(manifest.Name, "/", "|"), manifest.Version)))
+	encoder := json.NewEncoder(file)
+	_ = encoder.Encode(manifest)
+
 	return manifest, nil
 }
 
--- core/installer_test.go
+++ core/installer_test.go
@@ -3,14 +3,17 @@ package core
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"io"
 	"io/ioutil"
+	"strings"
 	"testing"
+
+	"github.com/smartystreets/assertions/should"
+	"github.com/smartystreets/gunit"
 
 	"bitbucket.org/smartystreets/satisfy/contracts"
 	"bitbucket.org/smartystreets/satisfy/fs"
-	"github.com/smartystreets/assertions/should"
-	"github.com/smartystreets/gunit"
 )
 
 func TestPackageInstallerFixture(t *testing.T) {
@@ -31,12 +34,44 @@ func (this *PackageInstallerFixture) Setup() {
 }
 
 func (this *PackageInstallerFixture) TestInstallManifest() {
-	originalManifest := contracts.Manifest{Name: "Gordon", Version: "1.2.3"}
-	raw, _ := json.Marshal(originalManifest)
-	this.downloader.Body = ioutil.NopCloser(bytes.NewReader(raw))
-	manifest, err := this.installer.InstallManifest(contracts.InstallationRequest{})
+	originalManifest := contracts.Manifest{Name: "Gor/don", Version: "1.2.3"}
+	this.downloader.prepareManifestDownload(originalManifest)
+	request := contracts.InstallationRequest{
+		DownloadRequest: contracts.DownloadRequest{
+			Bucket:   "bucket",
+			Resource: "resource",
+		},
+		LocalPath: "local/path",
+	}
+
+	manifest, err := this.installer.InstallManifest(request)
+
+	this.So(this.downloader.request, should.Resemble, contracts.DownloadRequest{
+		Bucket:   "bucket",
+		Resource: "resource",
+	})
 	this.So(manifest, should.Resemble, originalManifest)
 	this.So(err, should.BeNil)
+	reader := this.filesystem.Open("local/path/manifest_Gor|don_1.2.3.json")
+	decoder := json.NewDecoder(reader)
+	var localManifest contracts.Manifest
+	decoder.Decode(&localManifest)
+	this.So(localManifest, should.Resemble, originalManifest)
+}
+
+func (this *PackageInstallerFixture) TestInstallManifestDownloadError() {
+	downloadError := errors.New("something or other")
+	this.downloader.Error = downloadError
+	manifest, err := this.installer.InstallManifest(contracts.InstallationRequest{})
+	this.So(err, should.Equal, downloadError)
+	this.So(manifest, should.BeZeroValue)
+}
+
+func (this *PackageInstallerFixture) TestInstallManifestJsonDecodingError() {
+	this.downloader.prepareMalformedManifestDownload()
+	manifest, err := this.installer.InstallManifest(contracts.InstallationRequest{})
+	this.So(err, should.NotBeNil)
+	this.So(manifest, should.BeZeroValue)
 }
 
 //err2 := this.installer.InstallPackage(manifest, contracts.InstallationRequest{LocalPath:""})
@@ -44,10 +79,21 @@ func (this *PackageInstallerFixture) TestInstallManifest() {
 ////////////////////////////////////////
 
 type FakeDownloader struct {
-	Body  io.ReadCloser
-	Error error
+	Body    io.ReadCloser
+	Error   error
+	request contracts.DownloadRequest
 }
 
-func (this *FakeDownloader) Download(contracts.DownloadRequest) (io.ReadCloser, error) {
+func (this *FakeDownloader) Download(request contracts.DownloadRequest) (io.ReadCloser, error) {
+	this.request = request
 	return this.Body, this.Error
+}
+
+func (this *FakeDownloader) prepareManifestDownload(manifest contracts.Manifest) {
+	raw, _ := json.Marshal(manifest)
+	this.Body = ioutil.NopCloser(bytes.NewReader(raw))
+}
+
+func (this *FakeDownloader) prepareMalformedManifestDownload() {
+	this.Body = ioutil.NopCloser(strings.NewReader("malformed json"))
 }
