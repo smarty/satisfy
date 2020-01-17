a8804a1502b37723cb7e220512ba88ce413b6d97
Gordon Mathis <gordon@smartystreets.com>
2020-01-17 13:58:16 -0700

    Implemented install manifest and install package.

--- core/installer.go
+++ core/installer.go
@@ -1,8 +1,13 @@
 package core
 
 import (
+	"archive/tar"
+	"bytes"
+	"compress/gzip"
+	"crypto/md5"
 	"encoding/json"
 	"fmt"
+	"io"
 	"path/filepath"
 	"strings"
 
@@ -14,23 +19,78 @@ type PackageInstaller struct {
 	filesystem contracts.FileSystem
 }
 
+func NewPackageInstaller(downloader contracts.Downloader, filesystem contracts.FileSystem) *PackageInstaller {
+	return &PackageInstaller{downloader: downloader, filesystem: filesystem}
+}
+
 func (this *PackageInstaller) InstallManifest(request contracts.InstallationRequest) (manifest contracts.Manifest, err error) {
-	readcloser, err := this.downloader.Download(request.DownloadRequest)
+	body, err := this.downloader.Download(request.DownloadRequest)
 	if err != nil {
 		return manifest, err
 	}
-	decoder := json.NewDecoder(readcloser)
-	err = decoder.Decode(&manifest)
+
+	err = json.NewDecoder(body).Decode(&manifest)
 	if err != nil {
 		return manifest, err
 	}
-	file := this.filesystem.Create(filepath.Join(request.LocalPath, fmt.Sprintf("manifest_%s_%s.json", strings.ReplaceAll(manifest.Name, "/", "|"), manifest.Version)))
-	encoder := json.NewEncoder(file)
-	_ = encoder.Encode(manifest)
+
+	this.writeLocalManifest(request.LocalPath, manifest)
 
 	return manifest, nil
 }
 
-func NewPackageInstaller(downloader contracts.Downloader, filesystem contracts.FileSystem) *PackageInstaller {
-	return &PackageInstaller{downloader: downloader, filesystem: filesystem}
+func (this *PackageInstaller) writeLocalManifest(localPath string, manifest contracts.Manifest) {
+	file := this.filesystem.Create(composeManifestPath(localPath, manifest))
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "   ")
+	_ = encoder.Encode(manifest)
+}
+
+func (this *PackageInstaller) InstallPackage(manifest contracts.Manifest, request contracts.InstallationRequest) error {
+	body, err := this.downloader.Download(request.DownloadRequest)
+	if err != nil {
+		return err
+	}
+	hashReader := NewHashReader(body, md5.New())
+	gzipReader, err := gzip.NewReader(hashReader)
+	if err != nil {
+		return err
+	}
+	err = this.extractArchive(gzipReader, request)
+	if err != nil {
+		return err
+	}
+	actualChecksum := hashReader.Sum(nil)
+	if bytes.Compare(actualChecksum, manifest.Archive.MD5Checksum) != 0 {
+		// TODO: Clean up fileSystem
+		return fmt.Errorf("checksum mistmatch: %x != %x", actualChecksum, manifest.Archive.MD5Checksum)
+	}
+
+	return nil
+}
+
+func (this *PackageInstaller) extractArchive(gzipReader *gzip.Reader, request contracts.InstallationRequest) error {
+	tarReader := tar.NewReader(gzipReader)
+	for {
+		header, err := tarReader.Next()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		path := filepath.Join(request.LocalPath, header.Name)
+		writer := this.filesystem.Create(path)
+		_, err = io.Copy(writer, tarReader)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func composeManifestPath(localPath string,manifest contracts.Manifest) string {
+	cleanPackageName := strings.ReplaceAll(manifest.Name, "/", "|")
+	fileName := fmt.Sprintf("manifest_%s_%s.json", cleanPackageName, manifest.Version)
+	return filepath.Join(localPath, fileName)
 }
--- core/installer_test.go
+++ core/installer_test.go
@@ -1,7 +1,10 @@
 package core
 
 import (
+	"archive/tar"
 	"bytes"
+	"compress/gzip"
+	"crypto/md5"
 	"encoding/json"
 	"errors"
 	"io"
@@ -34,54 +37,132 @@ func (this *PackageInstallerFixture) Setup() {
 }
 
 func (this *PackageInstallerFixture) TestInstallManifest() {
-	originalManifest := contracts.Manifest{Name: "Gor/don", Version: "1.2.3"}
+	originalManifest := contracts.Manifest{Name: "Package/Name", Version: "1.2.3"}
 	this.downloader.prepareManifestDownload(originalManifest)
-	request := contracts.InstallationRequest{
+
+	manifest, err := this.installer.InstallManifest(this.installationRequest())
+
+	this.So(this.downloader.request, should.Resemble, this.installationRequest().DownloadRequest)
+	this.So(manifest, should.Resemble, originalManifest)
+	this.So(err, should.BeNil)
+	fileName := "local/path/manifest_Package|Name_1.2.3.json"
+	this.So(this.loadLocalManifest(fileName), should.Resemble, originalManifest)
+}
+
+func (this *PackageInstallerFixture) loadLocalManifest(fileName string) contracts.Manifest {
+	reader := this.filesystem.Open(fileName)
+	decoder := json.NewDecoder(reader)
+	var localManifest contracts.Manifest
+	_ = decoder.Decode(&localManifest)
+	return localManifest
+}
+
+func (this *PackageInstallerFixture) TestInstallManifestDownloadError() {
+	downloadError := errors.New("something or other")
+	this.downloader.Error = downloadError
+	manifest, err := this.installer.InstallManifest(this.installationRequest())
+	this.So(err, should.Equal, downloadError)
+	this.So(manifest, should.BeZeroValue)
+}
+
+func (this *PackageInstallerFixture) TestInstallManifestJsonDecodingError() {
+	this.downloader.prepareMalformedDownload()
+	manifest, err := this.installer.InstallManifest(this.installationRequest())
+	this.So(err, should.NotBeNil)
+	this.So(manifest, should.BeZeroValue)
+}
+
+func (this *PackageInstallerFixture) TestInstallPackageToLocalFileSystem() {
+	checksum := this.downloader.prepareArchiveDownload()
+
+	err := this.installer.InstallPackage(this.buildManifest(checksum), this.installationRequest())
+
+	this.So(err, should.BeNil)
+	this.So(this.filesystem.ReadFile("local/path/Hello/World"), should.Resemble, []byte("Hello World"))
+	this.So(this.filesystem.ReadFile("local/path/Goodbye/World"), should.Resemble, []byte("Goodbye World"))
+}
+
+func (this *PackageInstallerFixture) TestInstallPackageInvalidArchive() {
+	this.downloader.prepareMalformedDownload()
+
+	err := this.installer.InstallPackage(this.buildManifest(nil), this.installationRequest())
+
+	this.So(err, should.NotBeNil)
+	this.So(this.filesystem.Listing(), should.BeEmpty)
+}
+
+func (this *PackageInstallerFixture) TestInstallPackageDownloadError() {
+	this.downloader.Error = errors.New("i am an error")
+
+	err := this.installer.InstallPackage(this.buildManifest(nil), this.installationRequest())
+
+	this.So(err, should.NotBeNil)
+	this.So(this.filesystem.Listing(), should.BeEmpty)
+}
+
+func (this *PackageInstallerFixture) TestInstallPackageChecksumMismatch() {
+	this.downloader.prepareArchiveDownload()
+
+	err := this.installer.InstallPackage(this.buildManifest([]byte("mismatch")), this.installationRequest())
+
+	this.So(err, should.NotBeNil)
+	//this.So(this.filesystem.Listing(), should.BeEmpty) TODO
+}
+
+func (this *PackageInstallerFixture) buildManifest(checksum []byte) contracts.Manifest {
+	return contracts.Manifest{
+		Archive: contracts.Archive{
+			MD5Checksum: checksum,
+			Contents: []contracts.ArchiveItem{
+				{Path: "Hello/World"},
+				{Path: "Goodbye/World"},
+			},
+			CompressionAlgorithm: "gzip",
+		},
+	}
+}
+
+func (this *PackageInstallerFixture) installationRequest() contracts.InstallationRequest {
+	return contracts.InstallationRequest{
 		DownloadRequest: contracts.DownloadRequest{
 			Bucket:   "bucket",
 			Resource: "resource",
 		},
 		LocalPath: "local/path",
 	}
-
-	manifest, err := this.installer.InstallManifest(request)
-
-	this.So(this.downloader.request, should.Resemble, contracts.DownloadRequest{
-		Bucket:   "bucket",
-		Resource: "resource",
-	})
-	this.So(manifest, should.Resemble, originalManifest)
-	this.So(err, should.BeNil)
-	reader := this.filesystem.Open("local/path/manifest_Gor|don_1.2.3.json")
-	decoder := json.NewDecoder(reader)
-	var localManifest contracts.Manifest
-	decoder.Decode(&localManifest)
-	this.So(localManifest, should.Resemble, originalManifest)
 }
 
-func (this *PackageInstallerFixture) TestInstallManifestDownloadError() {
-	downloadError := errors.New("something or other")
-	this.downloader.Error = downloadError
-	manifest, err := this.installer.InstallManifest(contracts.InstallationRequest{})
-	this.So(err, should.Equal, downloadError)
-	this.So(manifest, should.BeZeroValue)
-}
-
-func (this *PackageInstallerFixture) TestInstallManifestJsonDecodingError() {
-	this.downloader.prepareMalformedManifestDownload()
-	manifest, err := this.installer.InstallManifest(contracts.InstallationRequest{})
-	this.So(err, should.NotBeNil)
-	this.So(manifest, should.BeZeroValue)
-}
-
-//err2 := this.installer.InstallPackage(manifest, contracts.InstallationRequest{LocalPath:""})
-
-////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
 
 type FakeDownloader struct {
 	Body    io.ReadCloser
 	Error   error
 	request contracts.DownloadRequest
+}
+
+func (this *FakeDownloader) prepareArchiveDownload() []byte {
+	hasher := md5.New()
+	writer := bytes.NewBuffer(nil)
+	multi := io.MultiWriter(hasher, writer)
+	compressor := gzip.NewWriter(multi)
+	archiveWriter := tar.NewWriter(compressor)
+
+	_ = archiveWriter.WriteHeader(&tar.Header{
+		Name: "Hello/World",
+		Size: int64(len("Hello World")),
+	})
+	_, _ = archiveWriter.Write([]byte("Hello World"))
+	_ = archiveWriter.WriteHeader(&tar.Header{
+		Name: "Goodbye/World",
+		Size: int64(len("Goodbye World")),
+	})
+	_, _ = archiveWriter.Write([]byte("Goodbye World"))
+	_ = archiveWriter.Close()
+	_ = compressor.Close()
+
+	this.Body = ioutil.NopCloser(bytes.NewReader(writer.Bytes()))
+
+	return hasher.Sum(nil)
 }
 
 func (this *FakeDownloader) Download(request contracts.DownloadRequest) (io.ReadCloser, error) {
@@ -94,6 +175,6 @@ func (this *FakeDownloader) prepareManifestDownload(manifest contracts.Manifest)
 	this.Body = ioutil.NopCloser(bytes.NewReader(raw))
 }
 
-func (this *FakeDownloader) prepareMalformedManifestDownload() {
+func (this *FakeDownloader) prepareMalformedDownload() {
 	this.Body = ioutil.NopCloser(strings.NewReader("malformed json"))
 }
